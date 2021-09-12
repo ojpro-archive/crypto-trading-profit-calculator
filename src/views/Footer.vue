2aca822c63dabd09a1f113d5f481ce51cefba04a
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-12 15:59:10 +0100

    Created Footer's component

---
+++ src/views/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-2">
+    <div class="text-center mt-4">
+      <span>
+        Created with 💖 by <a href="https://github.com/ojpro" class="text-blue-300 hover:text-purple-400">ojpro</a>(Oussama ELJabbari) - {{ currentYear() }}
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    currentYear: function () {
+      let date = new Date()
+      return date.getFullYear()
+    },
+  },
+}
+</script>
