75e60e59da84e890d164db4cc75bf60832ed8e1c
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-16 23:19:33 +0100

    Added Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="
+      bg-blue-500
+      text-white
+      rounded
+      focus:shadow-sm
+      focus:bg-blue-400
+      px-2
+      py-1.5
+      w-full
+      mt-3
+    "
+  >
+    {{ name }}
+  </button>
+</template>
+<script>
+export default {
+    props:['name']
+}
+</script>
