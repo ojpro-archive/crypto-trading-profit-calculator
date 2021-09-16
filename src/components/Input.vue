72a1dd6d93e1339e6025502af1a92ed4bcd17daf
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-16 23:19:10 +0100

    Added Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input
+      type="number"
+      class="
+        px-2
+        py-1.5
+        rounded
+        border border-gray-700
+        bg-gray-800
+        focus:shadow
+        w-full
+      "
+      :id="inputName"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: ['modelValue', 'inputName'],
+}
+</script>
